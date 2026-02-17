8df24f8e1f331752fcc9a13ad7623f915c4a00e5
eylulcankesici <eylulcankesici@gmail.com>
2026-02-17 14:24:23 +0300

    React ve Vite ile ilk commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+    <h1> Web Tasarimi ve Programalama</h1>
+    <h2>LAB-1</h2>
+    <p>Ad Soyad: Eylül Can Kesici</p>
+    <p>Ogrenci No: 235542003</p>
+    </div>
+  )
+}
+
+export default App
